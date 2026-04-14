740a663bb690281c6be08e508a5c73a677abe292
gitovsky-b <gitovsky1b@gmail.com>
2026-04-14 15:26:57 +0300

    Updated Windows build file

--- build_exe.bat
+++ build_exe.bat
@@ -9,16 +9,8 @@ if exist venv\Scripts\activate (
 
 pip install pyinstaller
 
-rmdir /s /q build dist 2>nul
-del /q *.spec 2>nul
-
-if not exist craft_app.spec (
-    echo Файл craft_app.spec не найден!
-    exit /b 1
-)
-
 echo Сборка приложения...
-pyinstaller craft_app.spec
+pyinstaller CraftHelper.spec
 
 if %errorlevel%==0 (
     echo Готово! Файл: dist\CraftHelper.exe
